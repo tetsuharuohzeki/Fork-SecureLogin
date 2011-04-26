7bdedc2b319a4743f1cd7da7ec1b1b8728e58af6
saneyuki_s <saneyuki.str@gmail.com>
2011-04-26 21:49:52 +0900

    Change default preference

--- defaults/preferences/secureLogin.js
+++ defaults/preferences/secureLogin.js
@@ -2,15 +2,15 @@ pref('extensions.secureLogin@blueimp.net.description', 'chrome://secureLogin/loc
 pref('extensions.secureLogin@blueimp.net.shortcut', 'chrome://secureLogin/locale/secureLogin.properties');
 pref('extensions.secureLogin@blueimp.net.hideToolsMenu',false);
 pref('extensions.secureLogin@blueimp.net.hideContextMenuItem',true);
-pref('extensions.secureLogin@blueimp.net.searchLoginsOnload',false);
+pref('extensions.secureLogin@blueimp.net.searchLoginsOnload',true);
 pref('extensions.secureLogin@blueimp.net.skipDuplicateActionForms',true);
 pref('extensions.secureLogin@blueimp.net.highlightColor','#FF9900');
 pref('extensions.secureLogin@blueimp.net.highlightOutlineStyle','solid');
 pref('extensions.secureLogin@blueimp.net.highlightOutlineWidth',1);
 pref('extensions.secureLogin@blueimp.net.highlightOutlineRadius',5);
 pref('extensions.secureLogin@blueimp.net.highlightStyle','');
-pref('extensions.secureLogin@blueimp.net.autoSubmitForm',true);
-pref('extensions.secureLogin@blueimp.net.javascriptProtection',false);
+pref('extensions.secureLogin@blueimp.net.autoSubmitForm',false);
+pref('extensions.secureLogin@blueimp.net.javascriptProtection',true);
 pref('extensions.secureLogin@blueimp.net.playLoginFoundSound',false);
 pref('extensions.secureLogin@blueimp.net.playLoginSound',false);
 pref('extensions.secureLogin@blueimp.net.exceptionList','');
