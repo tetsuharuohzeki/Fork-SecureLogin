d92646b77c13572acd0668810b9c20936f0330d7
saneyuki_s <saneyuki.s.snyk@gmail.com>
2011-09-23 01:51:03 +0900

    Fix secureLoginOverlay::tooltip().

--- content/secureLoginOverlay.js
+++ content/secureLoginOverlay.js
@@ -391,18 +391,18 @@ var secureLoginOverlay = {
 
 	tooltip: function (aEvent) {
 		// Check if document.tooltipNode exists and if it is shown above a valid node:
-		if (!document.tooltipNode
-		    || !document.tooltipNode.hasAttribute('tooltip')
-		    || !(document.tooltipNode.id == 'secureLoginButton')
-		) {
+		let tooltipNode = document.tooltipNode;
+		if (!tooltipNode || !tooltipNode.hasAttribute('tooltip') ||
+		    !(tooltipNode.getAttribute("id") === "secureLoginButton") ) {
 			// Don't show any tooltip:
 			aEvent.preventDefault();
 			return;
 		}
 
+		let service = this.service;
 		// Search for valid logins and outline login fields if not done automatically:
-		if (!this.service.prefs.getBoolPref('searchLoginsOnload')) {
-			this.service.searchLoginsInitialize(null, false);
+		if (!service.searchLoginsOnload) {
+			service.searchLoginsInitialize(null, false);
 		}
 
 		// hidden both boxes in tooltip:
@@ -418,24 +418,22 @@ var secureLoginOverlay = {
 				tooltip.removeChild(tooltip.firstChild);
 			}
 
-			if (this.service.secureLogins && this.service.secureLogins.length > 0) {
+			let secureLogins = service.secureLogins;
+			if (secureLogins && secureLogins.length > 0) {
 
 				// Hash list of unique action urls and number of logins:
-				let urlsArray = new Array();
+				let urlsArray = [];
 
 				// Go through the forms and find the unique action urls:
-				for (let i = 0; i < this.service.secureLogins.length; i++) {
-					let url = this.service.secureLogins[i].actionURI;
-					let foundInList = false;
+				for (let i = 0, l = secureLogins.length; i < l; i++) {
+					let url = secureLogins[i].actionURI;
 					// Check if the form action url is already in the list:
-					for (let j = 0; j < urlsArray.length; j++) {
-						if (urlsArray[j].url == url) {
-							// url already in the list, increase the counter:
-							foundInList = true;
-							urlsArray[j].count++;
-							break;
+					let foundInList = urlsArray.some(function(aElm){
+						if (aElm.url === url) {
+							aElm.count++;
+							return true
 						}
-					}
+					});
 					if (!foundInList) {
 						// Not in list, add the current url:
 						urlsArray.push({ url: url, count: 1,});
@@ -445,31 +443,24 @@ var secureLoginOverlay = {
 				if (urlsArray.length) {
 					// Add the url list:
 					let tooltipLoginURL = document.createElement('description');
-					tooltipLoginURL.setAttribute(
-					  'class',
-					  'secureLoginTooltipUrl'
-					);
+					tooltipLoginURL.setAttribute("class", "secureLoginTooltipUrl");
+
 					let spacer = document.createElement("spacer");
 					spacer.setAttribute("flex", "1");
-					let tooltipUrlCount = document.createElement('label');
-					tooltipUrlCount.setAttribute(
-					  'class',
-					  'secureLoginTooltipUrlCount'
-					);
-					for (let i = 0; i < urlsArray.length; i++) {
-						let hbox = document.createElement("hbox");
-						let descr = tooltipLoginURL.cloneNode(false);
-						let action = urlsArray[i];
-						descr.setAttribute(
-						  'value',
-						  action.url
-						);
-						let label = tooltipUrlCount.cloneNode(false);
-						label.setAttribute(
-						  'value',
-						  '('+ action.count +')'
-						);
 
+					let tooltipUrlCount = document.createElement('label');
+					tooltipUrlCount.setAttribute("class", "secureLoginTooltipUrlCount");
+
+					for (let i = 0, l = urlsArray.length; i < l; i++) {
+						let action = urlsArray[i];
+
+						let descr = tooltipLoginURL.cloneNode(false);
+						descr.setAttribute("value", action.url);
+
+						let label = tooltipUrlCount.cloneNode(false);
+						label.setAttribute("value", "("+ action.count +")");
+
+						let hbox = document.createElement("hbox");
 						hbox.appendChild(descr);
 						hbox.appendChild(spacer.cloneNode(false));
 						hbox.appendChild(label);
