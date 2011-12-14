d13dff9c4d4b8b5bc207dd40a76650debc75675a
saneyuki_s <saneyuki.s.snyk@gmail.com>
2011-12-15 00:52:25 +0900

    Fix preference namespace.

--- defaults/preferences/secureLogin.js
+++ defaults/preferences/secureLogin.js
@@ -1,6 +1,6 @@
 pref('extensions.secureLogin@blueimp.net.shortcut', 'chrome://secureLogin/locale/secureLogin.properties');
-pref('extensions.secureLogin.showDoorhangerLogin', true);
-pref('extensions.secureLogin.showDoorhangerLogin.dismissed', true);
+pref('extensions.secureLogin@blueimp.net.showDoorhangerLogin', true);
+pref('extensions.secureLogin@blueimp.net.showDoorhangerLogin.dismissed', true);
 pref('extensions.secureLogin@blueimp.net.searchLoginsOnload',true);
 pref('extensions.secureLogin@blueimp.net.skipDuplicateActionForms',true);
 pref('extensions.secureLogin@blueimp.net.highlightColor','#FF9900');
